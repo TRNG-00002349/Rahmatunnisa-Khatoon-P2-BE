720b68f72430a773816dfee08a6bc2c611a1ff85
Rahmatunnisa Khatoon <82597382+rahmat06-khatoon@users.noreply.github.com>
2026-01-25 22:59:57 -0600

    Project setup

---
+++ src/test/java/com/revature/RahmatunnisaKhatoonP2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahmatunnisaKhatoonP2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
